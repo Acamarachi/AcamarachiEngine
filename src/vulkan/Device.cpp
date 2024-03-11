a6993ea9b2742ac9d288dfe31bf7c629f085efc5
Quentin_T <quentin.tessier@epitech.eu>
2024-03-11 10:36:34 +0100

    Removing the Queue class in favor of a simple struct stored directly inside of the Device & Device creation + queue querying

--- src/vulkan/Device.cpp
+++ src/vulkan/Device.cpp
@@ -1,7 +1,7 @@
 #include "Device.hpp"
 
-Acamarachi::Vulkan::Device::Device(Acamarachi::Vulkan::Instance &instance)
-    : instance(instance)
+Acamarachi::Vulkan::Device::Device(Acamarachi::Vulkan::Instance &instance, Acamarachi::Vulkan::Surface& surface)
+    : instance(instance), surface(surface)
 { }
 
 Acamarachi::Vulkan::Device::~Device()
@@ -11,9 +11,6 @@ Acamarachi::Vulkan::Device::~Device()
 
 bool Acamarachi::Vulkan::Device::initialize(std::vector<const char *> requiredExtensions, std::vector<char const *> requiredValidationLayers)
 {
-    // (Void) suppress compilater warning
-    (void)requiredExtensions;
-    (void)requiredValidationLayers;
     std::vector<VkPhysicalDevice> physicalDevices;
     
     // Ask the instance for available physical devices
@@ -31,12 +28,66 @@ bool Acamarachi::Vulkan::Device::initialize(std::vector<const char *> requiredEx
         }
     }
 
+    if (physicalDevice == VK_NULL_HANDLE || !updateSurfaceCapabilities() || !findQueueFamilies()) {
+        return false;
+    }
+
+    float queuePriority = 1.0f;
+    VkDeviceQueueCreateInfo queueCreateInfo[3] = {};
+    queueCreateInfo[0].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo[0].queueCount = 1;
+    queueCreateInfo[0].queueFamilyIndex = graphicQueue.familyIndex;
+    queueCreateInfo[0].pQueuePriorities = &queuePriority;
+
+    queueCreateInfo[1].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo[1].queueCount = 1;
+    queueCreateInfo[1].queueFamilyIndex = graphicQueue.familyIndex;
+    queueCreateInfo[1].pQueuePriorities = &queuePriority;
+
+    queueCreateInfo[2].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo[2].queueCount = 1;
+    queueCreateInfo[2].queueFamilyIndex = graphicQueue.familyIndex;
+    queueCreateInfo[2].pQueuePriorities = &queuePriority;
+
+    VkPhysicalDeviceVulkan12Features featureVk1_2 = {};
+    featureVk1_2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES;
+    featureVk1_2.bufferDeviceAddress = VK_TRUE;
+    featureVk1_2.descriptorIndexing = VK_TRUE;
+
+    VkPhysicalDeviceVulkan13Features featureVk1_3 = {};
+    featureVk1_3.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES;
+    featureVk1_3.pNext = &featureVk1_2;
+    featureVk1_3.dynamicRendering = VK_TRUE;
+    featureVk1_3.synchronization2 = VK_TRUE;
+
+    VkPhysicalDeviceFeatures features = {};
+
+    VkDeviceCreateInfo deviceCreateInfo = {};
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    deviceCreateInfo.pNext = &featureVk1_3;
+    deviceCreateInfo.enabledExtensionCount = requiredExtensions.size();
+    deviceCreateInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    deviceCreateInfo.enabledLayerCount = requiredValidationLayers.size();
+    deviceCreateInfo.ppEnabledLayerNames = requiredValidationLayers.data();
+    deviceCreateInfo.queueCreateInfoCount = 3;
+    deviceCreateInfo.pQueueCreateInfos = queueCreateInfo;
+    deviceCreateInfo.pEnabledFeatures = &features;
+
+    VkResult res = vkCreateDevice(physicalDevice, &deviceCreateInfo, 0, &this->handle);
+    if (res != VK_SUCCESS) {
+        return false;
+    }
+
+    vkGetDeviceQueue(handle, graphicQueue.familyIndex, 0, &graphicQueue.handle);
+    vkGetDeviceQueue(handle, presentQueue.familyIndex, 0, &presentQueue.handle);
+    vkGetDeviceQueue(handle, transferQueue.familyIndex, 0, &transferQueue.handle);
+
     return true;
 }
 
 void Acamarachi::Vulkan::Device::deinitialize()
 {
-
+    vkDestroyDevice(handle, 0);
 }
 
 bool Acamarachi::Vulkan::Device::isPhysicalDeviceSuitable(VkPhysicalDevice device)
@@ -48,4 +99,35 @@ bool Acamarachi::Vulkan::Device::isPhysicalDeviceSuitable(VkPhysicalDevice devic
     vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
     return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU;
+}
+
+bool Acamarachi::Vulkan::Device::updateSurfaceCapabilities()
+{
+    return vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface.handle, &surfaceCapabilites) == VK_SUCCESS;
+}
+
+bool Acamarachi::Vulkan::Device::findQueueFamilies()
+{
+    uint32_t count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &count, 0);
+
+    std::vector<VkQueueFamilyProperties> properties = std::vector<VkQueueFamilyProperties>(count);
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &count, properties.data());
+
+    for (uint32_t i = 0; i < count; i++) {
+        VkQueueFamilyProperties current = properties[i];
+        VkBool32 supported = VK_FALSE;
+        vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, i, surface.handle, &supported);
+        if (graphicQueue.familyIndex == std::numeric_limits<uint32_t>::max() && current.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            graphicQueue.familyIndex = i;
+        } else if (transferQueue.familyIndex == std::numeric_limits<uint32_t>::max() && current.queueFlags & VK_QUEUE_TRANSFER_BIT) {
+            transferQueue.familyIndex = i;
+        } else if (presentQueue.familyIndex == std::numeric_limits<uint32_t>::max() && supported) {
+            presentQueue.familyIndex = i;
+        }
+    }
+
+    return graphicQueue.familyIndex == std::numeric_limits<uint32_t>::max()
+        || transferQueue.familyIndex == std::numeric_limits<uint32_t>::max()
+        || presentQueue.familyIndex == std::numeric_limits<uint32_t>::max();
 }
--- src/vulkan/Device.hpp
+++ src/vulkan/Device.hpp
@@ -3,22 +3,35 @@
 
 #include <vulkan/vulkan.h>
 #include <vector>
+#include <limits>
 
 #include "Instance.hpp"
+#include "Surface.hpp"
 
 namespace Acamarachi::Vulkan
 {
+
+    struct Queue {
+        uint32_t familyIndex = std::numeric_limits<uint32_t>::max();
+        VkQueue handle = VK_NULL_HANDLE;
+    };
 
     class Device
     {
     public:
         Instance& instance;
+        Surface& surface;
         VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
         VkDevice handle = VK_NULL_HANDLE;
+        VkSurfaceCapabilitiesKHR surfaceCapabilites = {};
+
+        Queue graphicQueue = {};
+        Queue presentQueue = {};
+        Queue transferQueue = {};
 
         // To create a device you need a working Vulkan instance, otherwise it will not work
         // as the devices a get through the instance
-        Device(Instance& instance);
+        Device(Instance& instance, Surface& surface);
         Device(const Device &) = delete;
         ~Device();
 
@@ -29,6 +42,10 @@ namespace Acamarachi::Vulkan
 
         // Perform a check to see if the device is what we want
         bool isPhysicalDeviceSuitable(VkPhysicalDevice device);
+        bool updateSurfaceCapabilities();
+
+    private:
+        bool findQueueFamilies();
 
         //TODO: ADD RATING FOR THE BEST PHYSICAL DEVICE
     };
