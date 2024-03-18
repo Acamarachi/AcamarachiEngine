162fc1179a009088183f0c006c45491a6ab7fd35
Maximilien Nowak <pro.mnowak@gmail.com>
2024-03-18 22:58:18 +0100

    split .hpp between proper .hpp and .cpp
    yeah, i run into the multiple definition error, didn't want to inline everythings. Thus i moved everythings.

--- src/maths/matrix.hpp
+++ src/maths/matrix.hpp
@@ -71,159 +71,47 @@ namespace Acamarachi::Maths
 
 	///---		ADDITION     ---///
 	//operator +=
-	void operator+=(mat2& a, mat2 o)
-	{
-		a[0] += o[0];
-		a[1] += o[1];
-	}
-
-	void operator+=(mat3& a, mat3 o)
-	{
-		a[0] += o[0];
-		a[1] += o[1];
-		a[2] += o[2];
-	}
-
-	void operator+=(mat4& a, mat4 o)
-	{
-		a[0] += o[0];
-		a[1] += o[1];
-		a[2] += o[2];
-		a[3] += o[3];
-	}
+	void operator+=(mat2& a, mat2 o);
+	void operator+=(mat3& a, mat3 o);
+	void operator+=(mat4& a, mat4 o);
 
 	//operator +=
-	void operator+=(mat2& a, float o)
-	{
-		a[0] += o;
-		a[1] += o;
-	}
-
-	void operator+=(mat3& a, float o)
-	{
-		a[0] += o;
-		a[1] += o;
-		a[2] += o;
-	}
-
-	void operator+=(mat4& a, float o)
-	{
-		a[0] += o;
-		a[1] += o;
-		a[2] += o;
-		a[3] += o;
-	}
+	void operator+=(mat2& a, float o);
+	void operator+=(mat3& a, float o);
+	void operator+=(mat4& a, float o);
 
 	//matrix + matrix
 	
-	mat2 operator+(mat2 a, mat2 b)
-	{
-		return mat2(a[0] + b[0], a[1] + b[1]);
-	}
-
-	mat3 operator+(mat3 a, mat3 b)
-	{
-		return mat3(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
-	}
-
-	mat4 operator+(mat4 a, mat4 b)
-	{
-		return mat4(a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]);
-	}
+	mat2 operator+(mat2 a, mat2 b);
+	mat3 operator+(mat3 a, mat3 b);
+	mat4 operator+(mat4 a, mat4 b);
 
 	//matrix + float
 
-	mat2 operator+(mat2 a, float b)
-	{
-		return mat2(a[0] + b, a[1] + b);
-	}
-
-	mat3 operator+(mat3 a, float b)
-	{
-		return mat3(a[0] + b, a[1] + b, a[2] + b);
-	}
-
-	mat4 operator+(mat4 a, float b)
-	{
-		return mat4(a[0] + b, a[1] + b, a[2] + b, a[3] + b);
-	}
+	mat2 operator+(mat2 a, float b);
+	mat3 operator+(mat3 a, float b);
+	mat4 operator+(mat4 a, float b);
 
 	///---		SUBSTRACTION     ---///
 	//operator -=
-	void operator-=(mat2& a, mat2 o)
-	{
-		a[0] -= o[0];
-		a[1] -= o[1];
-	}
-
-	void operator-=(mat3& a, mat3 o)
-	{
-		a[0] -= o[0];
-		a[1] -= o[1];
-		a[2] -= o[2];
-	}
-
-	void operator-=(mat4& a, mat4 o)
-	{
-		a[0] -= o[0];
-		a[1] -= o[1];
-		a[2] -= o[2];
-		a[3] -= o[3];
-	}
+	void operator-=(mat2& a, mat2 o);
+	void operator-=(mat3& a, mat3 o);
+	void operator-=(mat4& a, mat4 o);
 
 	//operator -=
-	void operator-=(mat2& a, float o)
-	{
-		a[0] -= o;
-		a[1] -= o;
-	}
-
-	void operator-=(mat3& a, float o)
-	{
-		a[0] -= o;
-		a[1] -= o;
-		a[2] -= o;
-	}
-
-	void operator-=(mat4& a, float o)
-	{
-		a[0] -= o;
-		a[1] -= o;
-		a[2] -= o;
-		a[3] -= o;
-	}
+	void operator-=(mat2& a, float o);
+	void operator-=(mat3& a, float o);
+	void operator-=(mat4& a, float o);
 
 	//matrix - matrix
 
-	mat2 operator-(mat2 a, mat2 b)
-	{
-		return mat2(a[0] - b[0], a[1] - b[1]);
-	}
-
-	mat3 operator-(mat3 a, mat3 b)
-	{
-		return mat3(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
-	}
-
-	mat4 operator-(mat4 a, mat4 b)
-	{
-		return mat4(a[0] - b[0], a[1] - b[1], a[2] - b[2], a[3] - b[3]);
-	}
+	mat2 operator-(mat2 a, mat2 b);
+	mat3 operator-(mat3 a, mat3 b);
+	mat4 operator-(mat4 a, mat4 b);
 
 	//matrix - float
 
-	mat2 operator-(mat2 a, float b)
-	{
-		return mat2(a[0] - b, a[1] - b);
-	}
-
-	mat3 operator-(mat3 a, float b)
-	{
-		return mat3(a[0] - b, a[1] - b, a[2] - b);
-	}
-
-	mat4 operator-(mat4 a, float b)
-	{
-		return mat4(a[0] - b, a[1] - b, a[2] - b, a[3] - b);
-	}
+	mat2 operator-(mat2 a, float b);
+	mat3 operator-(mat3 a, float b);
+	mat4 operator-(mat4 a, float b);
 }
--- src/maths/vector.hpp
+++ src/maths/vector.hpp
@@ -122,293 +122,93 @@ namespace Acamarachi::Maths
 	///---		ADDITION     ---///
 
 	//vector +=
-	void operator+=(vec2& a, vec2 o) {
-		a[0] += o[0];
-		a[1] += o[1];
-	}
-
-	void operator+=(vec3& a, vec3 o) {
-		a[0] += o[0];
-		a[1] += o[1];
-		a[2] += o[2];
-	}
-
-	void operator+=(vec4& a, vec4 o) {
-		a[0] += o[0];
-		a[1] += o[1];
-		a[2] += o[2];
-		a[3] += o[3];
-	}
+	void operator+=(vec2& a, vec2 o);
+	void operator+=(vec3& a, vec3 o);
+	void operator+=(vec4& a, vec4 o);
 
 	//vector +=
-	void operator+=(vec2& a, float o) {
-		a[0] += o;
-		a[1] += o;
-	}
-
-	void operator+=(vec3& a, float o) {
-		a[0] += o;
-		a[1] += o;
-		a[2] += o;
-	}
-
-	void operator+=(vec4& a, float o) {
-		a[0] += o;
-		a[1] += o;
-		a[2] += o;
-		a[3] += o;
-	}
+	void operator+=(vec2& a, float o);
+	void operator+=(vec3& a, float o);
+	void operator+=(vec4& a, float o);
 
 	//vector + vector
-	vec2 operator+(vec2 a, vec2 o)
-	{
-		return vec2(a[0] + o[0], a[1] + o[1]);
-	};
-
-	vec3 operator+(vec3 a, vec3 o)
-	{
-		return vec3(a[0] + o[0], a[1] + o[1], a[2] + o[2]);
-	};
-
-	vec4 operator+(vec4 a, vec4 o)
-	{
-		return vec4(a[0] + o[0], a[1] + o[1], a[2] + o[2], a[3] + o[3]);
-	};
+	vec2 operator+(vec2 a, vec2 o);
+	vec3 operator+(vec3 a, vec3 o);
+	vec4 operator+(vec4 a, vec4 o);
 
 	//Vector + float
 
-	vec2 operator+(vec2 a, float o)
-	{
-		return vec2(a[0] + o, a[1] + o);
-	};
-
-	vec3 operator+(vec3 a, float o)
-	{
-		return vec3(a[0] + o, a[1] + o, a[2] + o);
-	};
-
-	vec4 operator+(vec4 a, float o)
-	{
-		return vec4(a[0] + o, a[1] + o, a[2] + o, a[3] + o);
-	};
+	vec2 operator+(vec2 a, float o);
+	vec3 operator+(vec3 a, float o);
+	vec4 operator+(vec4 a, float o);
 
 	///---		SUBSTRACTION     ---///
 
 	//vector -=
-	void operator-=(vec2& a, vec2 o) {
-		a[0] -= o[0];
-		a[1] -= o[1];
-	}
-
-	void operator-=(vec3& a, vec3 o) {
-		a[0] -= o[0];
-		a[1] -= o[1];
-		a[2] -= o[2];
-	}
-
-	void operator-=(vec4& a, vec4 o) {
-		a[0] -= o[0];
-		a[1] -= o[1];
-		a[2] -= o[2];
-		a[3] -= o[3];
-	}
+	void operator-=(vec2& a, vec2 o);
+	void operator-=(vec3& a, vec3 o);
+	void operator-=(vec4& a, vec4 o);
 
 	//vector -=
-	void operator-=(vec2& a, float o) {
-		a[0] -= o;
-		a[1] -= o;
-	}
-
-	void operator-=(vec3& a, float o) {
-		a[0] -= o;
-		a[1] -= o;
-		a[2] -= o;
-	}
-
-	void operator-=(vec4& a, float o) {
-		a[0] -= o;
-		a[1] -= o;
-		a[2] -= o;
-		a[3] -= o;
-	}
+	void operator-=(vec2& a, float o);
+	void operator-=(vec3& a, float o);
+	void operator-=(vec4& a, float o);
 
 	//vector - vector
-	vec2 operator-(vec2 a, vec2 o)
-	{
-		return vec2(a[0] - o[0], a[1] - o[1]);
-	};
-
-	vec3 operator-(vec3 a, vec3 o)
-	{
-		return vec3(a[0] - o[0], a[1] - o[1], a[2] - o[2]);
-	};
-
-	vec4 operator-(vec4 a, vec4 o)
-	{
-		return vec4(a[0] - o[0], a[1] - o[1], a[2] - o[2], a[3] - o[3]);
-	};
+	vec2 operator-(vec2 a, vec2 o);
+	vec3 operator-(vec3 a, vec3 o);
+	vec4 operator-(vec4 a, vec4 o);
 
 	//Vector - float
 
-	vec2 operator-(vec2 a, float o)
-	{
-		return vec2(a[0] - o, a[1] - o);
-	};
-
-	vec3 operator-(vec3 a, float o)
-	{
-		return vec3(a[0] - o, a[1] - o, a[2] - o);
-	};
-
-	vec4 operator-(vec4 a, float o)
-	{
-		return vec4(a[0] - o, a[1] - o, a[2] - o, a[3] - o);
-	};
+	vec2 operator-(vec2 a, float o);
+	vec3 operator-(vec3 a, float o);
+	vec4 operator-(vec4 a, float o);
 
 	///---		MULTIPLICATION     ---///
 
 	//vector *=
-	void operator*=(vec2& a, vec2 o) {
-		a[0] *= o[0];
-		a[1] *= o[1];
-	}
-
-	void operator*=(vec3& a, vec3 o) {
-		a[0] *= o[0];
-		a[1] *= o[1];
-		a[2] *= o[2];
-	}
-
-	void operator*=(vec4& a, vec4 o) {
-		a[0] *= o[0];
-		a[1] *= o[1];
-		a[2] *= o[2];
-		a[3] *= o[3];
-	}
+	void operator*=(vec2& a, vec2 o);
+	void operator*=(vec3& a, vec3 o);
+	void operator*=(vec4& a, vec4 o);
 
 	//vector *=
-	void operator*=(vec2& a, float o) {
-		a[0] *= o;
-		a[1] *= o;
-	}
-
-	void operator*=(vec3& a, float o) {
-		a[0] *= o;
-		a[1] *= o;
-		a[2] *= o;
-	}
-
-	void operator*=(vec4& a, float o) {
-		a[0] *= o;
-		a[1] *= o;
-		a[2] *= o;
-		a[3] *= o;
-	}
+	void operator*=(vec2& a, float o);
+	void operator*=(vec3& a, float o);
+	void operator*=(vec4& a, float o);
 
 	//vector * vector
-	vec2 operator*(vec2 a, vec2 o)
-	{
-		return vec2(a[0] * o[0], a[1] * o[1]);
-	};
-
-	vec3 operator*(vec3 a, vec3 o)
-	{
-		return vec3(a[0] * o[0], a[1] * o[1], a[2] * o[2]);
-	};
-
-	vec4 operator*(vec4 a, vec4 o)
-	{
-		return vec4(a[0] * o[0], a[1] * o[1], a[2] * o[2], a[3] * o[3]);
-	};
+	vec2 operator*(vec2 a, vec2 o);
+	vec3 operator*(vec3 a, vec3 o);
+	vec4 operator*(vec4 a, vec4 o);
 
 	//Vector * float
 
-	vec2 operator*(vec2 a, float o)
-	{
-		return vec2(a[0] * o, a[1] * o);
-	};
-
-	vec3 operator*(vec3 a, float o)
-	{
-		return vec3(a[0] * o, a[1] * o, a[2] * o);
-	};
-
-	vec4 operator*(vec4 a, float o)
-	{
-		return vec4(a[0] * o, a[1] * o, a[2] * o, a[3] * o);
-	};
+	vec2 operator*(vec2 a, float o);
+	vec3 operator*(vec3 a, float o);
+	vec4 operator*(vec4 a, float o);
 
 	///---		DIVISION     ---///
 
 	//vector /=
-	void operator/=(vec2& a, vec2 o) {
-		a[0] /= o[0];
-		a[1] /= o[1];
-	}
-
-	void operator/=(vec3& a, vec3 o) {
-		a[0] /= o[0];
-		a[1] /= o[1];
-		a[2] /= o[2];
-	}
-
-	void operator/=(vec4& a, vec4 o) {
-		a[0] /= o[0];
-		a[1] /= o[1];
-		a[2] /= o[2];
-		a[3] /= o[3];
-	}
+	void operator/=(vec2& a, vec2 o);
+	void operator/=(vec3& a, vec3 o);
+	void operator/=(vec4& a, vec4 o);
 
 	//vector /=
-	void operator/=(vec2& a, float o) {
-		a[0] /= o;
-		a[1] /= o;
-	}
-
-	void operator/=(vec3& a, float o) {
-		a[0] /= o;
-		a[1] /= o;
-		a[2] /= o;
-	}
-
-	void operator/=(vec4& a, float o) {
-		a[0] /= o;
-		a[1] /= o;
-		a[2] /= o;
-		a[3] /= o;
-	}
+	void operator/=(vec2& a, float o);
+	void operator/=(vec3& a, float o);
+	void operator/=(vec4& a, float o);
 
 	//vector / vector
-	vec2 operator/(vec2 a, vec2 o)
-	{
-		return vec2(a[0] / o[0], a[1] / o[1]);
-	};
-
-	vec3 operator/(vec3 a, vec3 o)
-	{
-		return vec3(a[0] / o[0], a[1] / o[1], a[2] / o[2]);
-	};
-
-	vec4 operator/(vec4 a, vec4 o)
-	{
-		return vec4(a[0] / o[0], a[1] / o[1], a[2] / o[2], a[3] / o[3]);
-	};
+	vec2 operator/(vec2 a, vec2 o);
+	vec3 operator/(vec3 a, vec3 o);
+	vec4 operator/(vec4 a, vec4 o);
 
 	//Vector / float
 
-	vec2 operator/(vec2 a, float o)
-	{
-		return vec2(a[0] / o, a[1] / o);
-	};
-
-	vec3 operator/(vec3 a, float o)
-	{
-		return vec3(a[0] / o, a[1] / o, a[2] / o);
-	};
-
-	vec4 operator/(vec4 a, float o)
-	{
-		return vec4(a[0] / o, a[1] / o, a[2] / o, a[3] / o);
-	};
+	vec2 operator/(vec2 a, float o);
+	vec3 operator/(vec3 a, float o);
+	vec4 operator/(vec4 a, float o);
 
 }
