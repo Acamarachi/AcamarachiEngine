d75837667ab0301add593a9e4273676f3b018763
Quentin_T <quentin.tessier@epitech.eu>
2024-03-22 10:26:06 +0100

    Adding type size asserts and Range type

--- src/types.h
+++ src/types.h
@@ -18,8 +18,7 @@ typedef unsigned long long int u64;
 typedef float f32;
 typedef double f64;
 
-typedef int b32;
-typedef bool b8;
+typedef int bool32;
 
 #if ACAMARACHI_ARCH_32BIT
     typedef unsigned int size_t;
@@ -28,3 +27,20 @@ typedef bool b8;
     typedef unsigned long long int size_t;
     typedef long long int isize_t;
 #endif
+
+typedef struct Range64 {
+    u64 offset;
+    u64 size;
+};
+
+typedef struct Range32 {
+    u32 offset;
+    u32 size;
+};
+
+static_assert(sizeof(u8) == 1, "Expected u8 to be 1 byte.");
+static_assert(sizeof(u16) == 2, "Expected u16 to be 2 byte.");
+static_assert(sizeof(u32) == 4, "Expected u32 to be 4 byte.");
+static_assert(sizeof(u64) == 8, "Expected u64 to be 8 byte.");
+static_assert(sizeof(f32) == 4, "Expected f32 to be 4 byte."); 
+static_assert(sizeof(f64) == 8, "Expected f64 to be 8 byte."); 
